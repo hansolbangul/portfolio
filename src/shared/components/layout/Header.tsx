92c023e8db14a06e3d9761e04fdab73d4c2c9566
hansolbangul <ruaenddl981028@naver.com>
2024-12-19 00:41:39 +0900

    [Fix] 헤더 z-index 수정

--- src/shared/components/layout/Header.tsx
+++ src/shared/components/layout/Header.tsx
@@ -61,7 +61,7 @@ export default function Header() {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 ${
+      className={`fixed top-0 left-0 right-0 z-40 ${
         theme === "light"
           ? "bg-white/80 border-gray-200"
           : "bg-gray-900/80 border-gray-800"
